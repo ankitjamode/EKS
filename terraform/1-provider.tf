feaa5448ae35a21c54df91eeeaf01550c9f674ad
Ankit Punjaji Jamode <ankit@vipul.local>
2024-10-22 03:29:26 +0530

    3rd

--- terraform/1-provider.tf
+++ terraform/1-provider.tf
@@ -5,8 +5,10 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "terrafrom-backend-ankit"  
-    key            = "terraform/state"                           
-    dynamodb_table = "terraform-lock"                                       
+    key            = "terraform/state"            
+    region         = "us-east-1"                  
+    dynamodb_table = "terraform-lock"              
+    encrypt        = true                          
   }
 
 
